4b3bde5a4bef0c8adea73c77215f6382987adc29
DLLGLI93 <giulia.dallaricca@gmail.com>
2018-10-31 14:32:00 +0100

    fine utente

--- src/main/java/it/supermarket/vo/Utente.java
+++ src/main/java/it/supermarket/vo/Utente.java
@@ -1,5 +1,34 @@
 package it.supermarket.vo;
 
 public class Utente {
-
+	private Integer id;
+	private String username;
+	private String password;
+	private float credito;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public float getCredito() {
+		return credito;
+	}
+	public void setCredito(float credito) {
+		this.credito = credito;
+	}
+	
+	
 }
